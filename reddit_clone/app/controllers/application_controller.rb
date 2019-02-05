b7efd6e4de3f47d2b030dfd314343954f2f98e7b
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-02-05 10:36:42 -0800

    Finish phase 1

--- reddit_clone/app/controllers/application_controller.rb
+++ reddit_clone/app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :signed_in?
+
+    def sign_in(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def sign_out
+        current_user.reset_session_token!
+        session[:session_token] = nil
+    end
+
+    def current_user
+        User.find_by(session: session[:session_token])
+    end
+
+    def signed_in?
+        !!current_user
+    end
+
+    def require_sign_in
+        redirect_to new_session_url unless signed_in?
+    end
 end
